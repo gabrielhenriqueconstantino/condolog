64a5fd871bc4162defb81b772ea4c8d90676ed60
gabrielhenriqueconstantino <gabrielconstantinogh@gmail.com>
2025-08-24 17:43:21 -0300

    erro de build do netlify - problemas com componente dashboard.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LayoutLanding from "./pages/landing/layout/LayoutLanding";
 import Login from "./pages/admin/Login/Login";
-import Dashboard from './pages/admin/Dashboard/layout/Dashboard';
+import Dashboard from './pages/admin/Dashboard/layout/Dashboard.jsx';
 
 function App() {
   return (
